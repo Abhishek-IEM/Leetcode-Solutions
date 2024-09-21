23308619215a645b94e19788366ec3c159dcdac5
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-09-21 09:18:32 +0530

    Time: 11 ms (68.21%), Space: 10 MB (96.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,6 +5,7 @@ public:
         int first = 0, second = 0, len = 0;
         while(second < s.size())
         {
+            //Repeating Character
             while(count[s[second]])
             {
                 count[s[first]] = 0;
