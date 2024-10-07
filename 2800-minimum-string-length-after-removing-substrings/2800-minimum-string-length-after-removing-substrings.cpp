7381297e4cda3cd29b9af195582ee0b44a8faf96
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-10-07 10:04:49 +0530

    Time: 12 ms (15.77%), Space: 11.6 MB (31.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -3,17 +3,15 @@ public:
     int minLength(string s) {
         int n = s.length();
         stack<char> st;
-          // Iterate through the string
-    for (int i = 0; i < n; i++) {
-        // Check if the top two elements form the pairs 'AB' or 'CD'
-        if (!st.empty() && ((st.top() == 'A' && s[i] == 'B') || (st.top() == 'C' && s[i] == 'D'))) {
-            st.pop();  // Remove the top if pair is found
-        } else {
-            st.push(s[i]);  // Otherwise, push the current character
+        for (int i = 0; i < n; i++) {
+            if (!st.empty() && ((st.top() == 'A' && s[i] == 'B') || (st.top() == 'C' && s[i]    ==    'D'))){
+                st.pop();  
+            } else {
+                st.push(s[i]);  
+            }
         }
-    }
-    
-    // The size of the stack will be the minimized length
-    return st.size();
-    }
+        
+        // The size of the stack will be the minimized length
+        return st.size();
+        }
 };
