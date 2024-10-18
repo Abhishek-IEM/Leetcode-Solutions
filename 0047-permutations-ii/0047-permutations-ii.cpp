84faafb1e3ad693a5671798e94c343519ace9bca
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-10-18 10:34:16 +0530

    Time: 9 ms (38.21%), Space: 14.5 MB (21.72%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void permut(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp,  vector<bool> & visited)
+    {
+        if(visited.size() == temp.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        unordered_set<int> seen;
+        for(int i = 0; i < visited.size(); i++)
+        {
+            if(visited[i] || seen.find(nums[i]) != seen.end()) continue;
+            seen.insert(nums[i]);
+
+            if(visited[i] == 0)
+            {
+                visited[i] = 1;
+                temp.push_back(nums[i]);
+                permut(nums, ans, temp, visited);
+                visited[i] = 0;
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<bool> visited(nums.size(), 0);
+        vector<int> temp;
+        permut(nums, ans, temp, visited);
+        return ans;
+    }
+};
