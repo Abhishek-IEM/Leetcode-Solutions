78da4c2e97eb01ca368324d1cf45b1362d870aa2
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-01-25 18:43:48 +0530

    Time: 0 ms (100%), Space: 13.7 MB (57.01%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -11,26 +11,33 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root)
-    {
-        if(!root) return 0;
-        return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-
-    bool dfs(TreeNode* root, int i, int totalNodes)
-    {
-        if(!root) return true;
-
-        if(i > totalNodes) return false;
-
-        return dfs(root->left, 2 * i, totalNodes) && dfs(root->right, 2 * i + 1, totalNodes);
-    }
-
     bool isCompleteTree(TreeNode* root) {
-        int totalNodes = countNodes(root);
-
-        int i = 1;
-
-        return dfs(root, i, totalNodes);
+        //Using BFS
+        
+        queue<TreeNode*> que;
+        
+        que.push(root);
+        
+        bool past = false; //kya aapne past me NULL node dekha hai 
+        
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            
+            if(node == NULL) {
+                past = true;
+            } else {
+                if(past == true) {
+                    return false;
+                }
+                
+                que.push(node->left);
+                que.push(node->right);
+            }
+        }
+        
+        return true;
     }
 };
+
+
