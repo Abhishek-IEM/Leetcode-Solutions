5a4e40c2165adff2d20663c284caba2914b817bc
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-08-10 18:17:04 +0530

    Time: 0 ms (100%), Space: 8 MB (86.43%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getCount(int n)
+    {
+        int num = 0;
+
+        while(n)
+        {
+            num += pow(10, n % 10);
+            n /= 10;
+        }
+
+        return num;
+    }
+
+    bool reorderedPowerOf2(int n) {
+        int input = getCount(n);
+
+        for(int p = 0; p <= 29; p++)
+        {
+            if(input == getCount(1 << p)) return true;
+        }
+
+        return false;
+    }
+};
