f0fe09cf567771206e60e00d95ce4b9207d0c0c9
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-08-20 17:09:45 +0530

    Time: 11 ms (48.97%), Space: 31.1 MB (16.12%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>& matrix, int i, int j, vector<vector<int>>& dp)
-    {
-        if(i >= matrix.size() || j >= matrix[0].size()) return 0;
-
-        if(matrix[i][j] == '0') return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(matrix, i + 1, j, dp);
-        int diag = solve(matrix, i + 1, j + 1, dp);
-        int below = solve(matrix, i, j + 1, dp);
-
-        return dp[i][j] = 1 + min({right, diag, below});
-    }
-    
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-
-        int ans = 0, maxAns = 0;
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(matrix[i][j] == '1'){
-                    ans = solve(matrix, i, j, dp);
-                    maxAns = max(maxAns, ans);
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        int maxSide = 0;
+        
+        // Fill dp
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; // first row/col
+                    } else {
+                        dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                    }
+                    maxSide = max(maxSide, dp[i][j]);
                 }
             }
         }
-
-        return maxAns * maxAns;
+        
+        return maxSide * maxSide; // area
     }
 };
